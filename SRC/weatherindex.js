f975e3586b398ae1594378d959b1b2b3f1b8a472
Ewshayla8606 <EWSHAYLA3@HOTMAIL.COM>
2025-10-25 21:51:49 -0400

    added the search engine for the city to display the temperature of that city.

--- SRC/weatherindex.js
+++ SRC/weatherindex.js
@@ -1,9 +1,30 @@
+function RefreshWeather (response){
+    let temperatureElement=document.querySelector("#temperature");
+    let temperature=response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    
+    temperatureElement.innerHTML=Math.round(temperature);
+    
+
+    
+}
+
+
+function searchCity (city){
+
+    let apiKey = "a43b704dc34352oa5c01d16337a4fct8";
+    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(RefreshWeather);
+}
+
 function handleSearchSubmit(event){
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
 
+
+
+searchCity(searchInput.value);
 
 }
 
